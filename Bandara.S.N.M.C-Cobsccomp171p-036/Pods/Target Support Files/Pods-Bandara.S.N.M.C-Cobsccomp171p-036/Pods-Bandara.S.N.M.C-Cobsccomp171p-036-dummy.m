8386ac530d878469683c8e9cdae956bd85c95975
Chathurika Bandara <chathurikabandara0701@gmail.com>
2019-05-17 17:29:12 +0530

    Pod initialisation

---
+++ Bandara.S.N.M.C-Cobsccomp171p-036/Pods/Target Support Files/Pods-Bandara.S.N.M.C-Cobsccomp171p-036/Pods-Bandara.S.N.M.C-Cobsccomp171p-036-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bandara_S_N_M_C_Cobsccomp171p_036 : NSObject
+@end
+@implementation PodsDummy_Pods_Bandara_S_N_M_C_Cobsccomp171p_036
+@end
---
+++ Bandara.S.N.M.C-Cobsccomp171p-036/Pods/Target Support Files/Pods-Bandara.S.N.M.C-Cobsccomp171p-036Tests/Pods-Bandara.S.N.M.C-Cobsccomp171p-036Tests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Bandara_S_N_M_C_Cobsccomp171p_036TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Bandara_S_N_M_C_Cobsccomp171p_036TestsVersionString[];
+
