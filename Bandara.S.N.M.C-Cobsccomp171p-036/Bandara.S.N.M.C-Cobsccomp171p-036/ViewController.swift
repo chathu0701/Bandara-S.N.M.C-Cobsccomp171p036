29f18f055a2aae788678cade90f85529d8101a84
Chathurika Bandara <chathurikabandara0701@gmail.com>
2019-05-17 13:31:28 +0530

    Initial setup
    project initial setup

---
+++ Bandara.S.N.M.C-Cobsccomp171p-036/Bandara.S.N.M.C-Cobsccomp171p-036/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bandara.S.N.M.C-Cobsccomp171p-036
+//
+//  Created by MacBook on 5/17/19.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
